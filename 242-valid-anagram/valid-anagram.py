615078df8d16cdf6edac994128f75282f356929e
Jagruthi Sachin Poondrrikaksha <173657547+jagruthi315@users.noreply.github.com>
2026-07-25 23:35:24 +0530

    Time: 17 ms (70.89%) | Memory: 14.2 MB (14.97%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,6 +5,29 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+          
+
+        if len(s) != len(t):
+            return False
+
+        freq = {}
+
+        for i in range(len(s)):
+            if s[i] not in freq:
+                freq[s[i]] = 1
+            else:
+                freq[s[i]] += 1
+
+            if t[i] not in freq:
+                freq[t[i]] = -1
+            else:
+                freq[t[i]] -= 1
+
+        for value in freq.values():
+            if value != 0:
+                return False
+
+        return True   
 
         if len(s) != len(t):
             return False
@@ -27,4 +50,3 @@ class Solution(object):
                 return False
 
         return True
-        
