6e10308879264118985c5b540dff9c3e42936258
Jagruthi Sachin Poondrrikaksha <173657547+jagruthi315@users.noreply.github.com>
2026-08-09 22:36:23 +0530

    Time: 23 ms (8.50%) | Memory: 14.3 MB (20.83%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -4,10 +4,87 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        res =[]
-    
-        for i in range(len(nums)):
-            val = lambda x:x*x
-            res.append(val(nums[i]))
+
         
-        return sorted(res)
+        pos=[]
+        neg=[]
+        main=[]
+      
+        
+    
+        for i in range (len(nums)):
+            if nums[i]>=0:
+               pos.append(nums[i])
+            
+            if nums[i]<0:
+               neg.append(nums[i])
+            
+        if len(pos)==0:
+            for i in range(len(neg)):
+                val = lambda x: x * x
+                main.append(val(neg[i]))
+                i+=1
+                  
+            main = main[::-1]
+            return main 
+            
+        if len(neg)==0:
+            for i in range(len(pos)):
+                val = lambda x: x * x
+                main.append(val(pos[i]))
+                i+=1
+            return main
+
+
+            # if len(neg) and len(pos)!=0:
+        else:
+            neg = [x*x for x in neg][::-1]
+            pos = [x*x for x in pos]
+
+
+               
+
+                  
+        m =len(neg)
+        n = len(pos)
+        i = j = 0
+           
+
+        while(i<m and j<n):
+            
+            if neg[i]<= pos[j]:
+                        
+                main.append(neg[i])
+                i+=1
+                    
+            else:
+                       
+                main.append(pos[j])
+                j+=1
+                    
+
+        while(j<n):
+                
+            main.append(pos[j])
+            j+=1
+                
+
+        while(i<m):
+                
+            main.append(neg[i])
+            i+=1
+
+           
+        return main       
+
+        
+
+
+
+        
+                
+               
+
+                
+                
+                   
